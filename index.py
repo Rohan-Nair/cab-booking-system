1395f4a9d93c736f32a334308215e684b598bdde
Rohan-Nair <rohan.12110392@lpu.in>
2022-11-12 09:38:30 +0530

    register user form completed gui database not integrated

--- index.py
+++ index.py
@@ -33,6 +33,8 @@ class user:
         newroot.config(bg="black")
         # on clicking the login button
         def clicklogin():
+            username=StringVar()
+            password=StringVar()
             db = mysql.connector.connect(host="localhost", user="root", password="1234")
             db_cursor = db.cursor()
 
@@ -112,28 +114,136 @@ class user:
 
 
     def new_user(self):
-        db = mysql.connector.connect(host="localhost", user="root", password="1234")
-        db_cursor = db.cursor()
+        # db = mysql.connector.connect(host="localhost", user="root", password="1234")
+        # db_cursor = db.cursor()
 
-        # check if the username entered by the user already exists in the database in that case show and error
-        findusername_query = "SELECT * FROM user WHERE USERNAME = ?"
-        askusername = self.username.get()
-        db_cursor.execute(findusername_query, [askusername])
-        if db_cursor.fetchall():
-            # checking if the username already exists in the table
-            msg.showerror("Error", "Username already taken!!")
+        # # check if the username entered by the user already exists in the database in that case show and error
+        # findusername_query = "SELECT * FROM user WHERE USERNAME = ?"
+        # askusername = self.username.get()
+        # db_cursor.execute(findusername_query, [askusername])
+        # if db_cursor.fetchall():
+        #     # checking if the username already exists in the table
+        #     msg.showerror("Error", "Username already taken!!")
 
-        # if the username doesnt already exist show that the account has been created and go to the login page
-        else:
-            # adding the new user to the database
-            addnewuser_query = "INSERT INTO USER(USERNAME, PASSWORD) VALUES(?,?)"
-            db_cursor.execute(addnewuser_query, [askusername, self.password.get()])
-            db.commit()
-            db.close()
-            # user added
-            msg.showinfo("Complete!", "Account Created!!")
-            # go to login
-            self.login()
+        # # if the username doesnt already exist show that the account has been created and go to the login page
+        # else:
+        #     # adding the new user to the database
+        #     addnewuser_query = "INSERT INTO USER(USERNAME, PASSWORD) VALUES(?,?)"
+        #     db_cursor.execute(addnewuser_query, [askusername, self.password.get()])
+        #     db.commit()
+        #     db.close()
+        #     # user added
+        #     msg.showinfo("Complete!", "Account Created!!")
+        #     # go to login
+        #     user(root)
+        newroot = Tk()
+        newroot.title("Register User")
+        # root.forget()
+        newroot.config(bg="black")
+
+        def clearonclick1(): #to clear existing text in username section
+            usernameentry.delete(0, "end")
+        def clearonclick2(): #to clear existing text in password section
+            passwordentry.delete(0, "end")
+        def clearonclick3(): #to clear existing text in mobile number section
+            mobileentry.delete(0,"end")
+        def clearonclick4(): #to clear existing text in email section
+            emailentry.delete(0,"end")
+
+        # new user heading 
+        Label(
+            newroot,
+            text="Welcome to Cab Booking System",
+            font=("Lato 18 underline"),
+            pady=2,  
+            fg="light blue", 
+            bg="black", 
+            width=35,
+        ).grid(row=0, column=0, columnspan=2, pady=10)
+
+        # username
+        Label(
+            newroot, 
+            text="Username: ", 
+            font = ("Lato", 15), 
+            pady=2, 
+            fg="light blue", 
+            bg="black"
+        ).grid(row=1, column=0, pady=10, sticky="w", padx=2)
+        usernameentry = Entry(newroot, width=30, font=("Lato", 15), background="light blue")
+        usernameentry.insert(0, "Enter your username...")
+        usernameentry.bind("<FocusIn>", clearonclick1)
+        usernameentry.grid(row=1, column=1, pady=10, padx=2)
+
+        # password 
+        Label(
+            newroot, 
+            text="Password: ",
+            font=("Lato", 15), 
+            pady=2, 
+            fg="light blue", 
+            bg="black",
+        ).grid(row=2, column=0, pady=10, padx=2, sticky="w")
+        passwordentry = Entry(newroot, width=30, font=("Lato", 15), background="light blue")
+        passwordentry.insert(0, "Enter your password...")
+        passwordentry.bind("<FocusIn>", clearonclick2)
+        passwordentry.grid(row=2, column=1, pady=10, padx=2)
+
+        # gender drop down 
+        Label(
+            newroot,
+            text="Gender: ",
+            font=("Lato", 15),
+            pady=2,
+            fg="light blue", 
+            bg="black", 
+        ).grid(row=3, column=0, sticky="w", padx=2)
+        gender=StringVar()
+        gender.set("--Select--")
+        genderdrop = OptionMenu(newroot, gender, "Male", "Female", "Other")
+        genderdrop.config(fg="light blue", bg="black", font=("Lato", 13), activebackground="black", activeforeground="light blue")
+        genderdrop.grid(row=3, column=1, pady=10, sticky="w", padx=5)
+
+        # mobile number 
+        Label(
+            newroot, 
+            text="Mobile: ", 
+            font=("Lato", 15), 
+            pady=2, 
+            fg="light blue", 
+            bg="black"
+        ).grid(row=4, column=0, pady=10, padx=2, sticky="w")
+        mobileentry=Entry(newroot, width=30, font=("Lato", 15), background="light blue")
+        mobileentry.insert(0, "Enter your mobile number...")
+        mobileentry.bind("<FocusIn>", clearonclick3)
+        mobileentry.grid(row=4, column=1,pady=10, padx=2)
+
+        # email id 
+        Label(
+            newroot, 
+            text="Email ID: ",
+            font=("Lato", 15),
+            pady=2,
+            fg="light blue",
+            bg="black", 
+        ).grid(row=5, column=0, pady=10, padx=2, sticky="w")
+        emailentry=Entry(newroot,width=30, font=("Lato", 15), background="light blue")
+        emailentry.insert(0,"Enter your Email ID...")
+        emailentry.bind("<FocusIn>", clearonclick4)
+        emailentry.grid(row=5, column=1, pady=10, padx=2)
+
+        # Register button 
+        Button(
+            newroot,
+            text="Submit",
+            font=("Lato", 16),
+            pady=2, 
+            fg="light blue", 
+            bg="black", 
+        ).grid(row=6, column=0, columnspan=2, pady=10)
+
+        newroot.mainloop()
+
 
     def available_routes(self):
         root.forget()
